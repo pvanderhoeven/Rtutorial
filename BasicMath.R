8382b6d7f8e69f7e98579f4e672e8db92c5c4252
Peter van der Hoeven <peter@ioma.nl>
2016-03-29 23:35:49 +0200

    init

---
+++ BasicMath.R
@@ -0,0 +1,33 @@
+1+1
+1+2+3
+
+x <- 2
+
+a <- b <- 3
+assign("j",4)
+
+
+c(One="a", Two="c", Three="d")
+w <- 1:3
+w
+names(w) <- c("a","b","c")
+qFactor <- factor(q)
+qFactor
+as.numeric(qFactor)
+v <- c(1, 2, NA, 3, 8, NA, 3, 3 )
+v
+is.na(v)
+
+p <- c("aap",NA, "noot", "mies")
+p
+
+mean(x)
+sum(x)
+nchar(x)
+mean(x=x, trim = .9)
+mean(x=x, na.rm = TRUE)
+
+x[c(2,6)] <- NA
+x
+mean(x)
+mean(x=x, na.rm = TRUE)
